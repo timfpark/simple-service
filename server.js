85827534a3b35b07b091b91152e65e661f1126ae
Tim Park <timfpark@gmail.com>
2018-05-01 10:56:04 -0700

    inject a secret passphrase from environmental variables

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express'),
-      app = express();
+    app = express();
 
 let instanceId = Math.floor(Math.random() * 100000);
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', process.env.PORT || 5000);
 
 app.get('/', function(request, response) {
-  response.send(`Hello from instance id ${instanceId} at ${new Date()}`);
+    response.send(
+        `The secret passphrase is ${
+            process.env.SECRET_PASSPHRASE
+        } from instance id ${instanceId} at ${new Date()}`
+    );
 });
 
 app.listen(app.get('port'), function() {
-  console.log("App is running at localhost:" + app.get('port'))
+    console.log('app is running on port ' + app.get('port'));
 });
